86c3694a5a6d8f012be64b8f519ab81f2da5b36c
michihidesugito <Michihides@gmail.com>
2023-09-19 18:18:37 +0200

    kommentarer tillagda

--- siteLogic.js
+++ siteLogic.js
@@ -1,3 +1,4 @@
+//Funktion som frågar om grisar i gris.html
 function grisar() {
     var question = "Gillar du grisar??? \n Tryck OK för JA";
     if (confirm(question) == true) {
@@ -8,6 +9,7 @@ function grisar() {
     }
 }
 
+//Funktion som spelar upp en ljud fil OM man svarar rätt i anka.html
 function ankor() {
     var audio = new Audio("sounds/ducks.wav");
     var question = prompt("Hur låter ankor?");
@@ -19,6 +21,7 @@ function ankor() {
     }
 }
 
+//Funktion som ber en räkna hur många kossor man ser i kossa.html
 function ko() {
     var question = prompt("Hur många kor ser du totalt på sidan???");
     if ((question) == 2) {
@@ -29,16 +32,19 @@ function ko() {
     }
 }
 
+//Funktion som gömmer en textdel tills man trycker på en knapp i höns.html
 function kycklingar() {
     var text = document.getElementById("hiddenText");
     text.style.display = "block";
 }
 
+//Funktion som gömmer en textdel tills man trycker på hint knapp i anka.html
 function ankaHint() {
     var text = document.getElementById("buttonAnkaText");
     text.style.display = "block";
 }
 
+//Funktion som ber en addera 2 tal och skriva ut svaret i hund.html
 function hund() {
     var question = parseInt(prompt("Hundar är smarta, är DU? \nSkriv in ett tal mellan 1-20"));
     var questionTwo = parseInt(prompt("Skriv in ett till tal mellan 1-20"));
